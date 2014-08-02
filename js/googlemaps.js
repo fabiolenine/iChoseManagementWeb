ac49c18440aea107e9b6eb93d7c57d9eb193eed5
fabiolenine <fabiolenine@icloud.com>
2014-08-02 05:34:25 -0300

    Fornecedor
    Mapa

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,16 +1,38 @@
+var myCenter=new google.maps.LatLng(53, -1.33);
+var marker=new google.maps.Marker({
+    position:myCenter
+});
+
 function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-}
+  var mapProp = {
+      center:myCenter,
+      zoom: 14,
+      draggable: false,
+      scrollwheel: false,
+      mapTypeId:google.maps.MapTypeId.SATELLITE
+  };
+  
+  var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
+  marker.setMap(map);
+    
+  google.maps.event.addListener(marker, 'click', function() {
+      
+    infowindow.setContent(contentString);
+    infowindow.open(map, marker);
+    
+  }); 
+};
+
+$('.btnmap').click(function(){
+
+  	initialize();
+
+});
 
 function loadScript() {
   var script = document.createElement("script");
   script.type   = "text/javascript";
-  script.src    = "https://maps.googleapis.com/maps/api/js?key=AIzaSyB3g1x-yuUasJMAZ96tyi18_IUq99h_wRc&sensor=TRUE&callback=initialize";
+  script.src    = "http://maps.googleapis.com/maps/api/js?key=AIzaSyB3g1x-yuUasJMAZ96tyi18_IUq99h_wRc&sensor=TRUE";
   document.body.appendChild(script);
 }
 
