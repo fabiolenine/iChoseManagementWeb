0963c28ab6ac576da44439f8fc3977bde7973f3a
fabiolenine <fabiolenine@icloud.com>
2014-10-15 21:22:29 -0300

    Local
    Regex

--- js/mascaraValidacao.js
+++ js/mascaraValidacao.js
@@ -7,14 +7,6 @@ function MascaraCNPJ(cnpj){
         return formataCampo(cnpj, '00.000.000/0000-00', event);
 }
 
-//adiciona mascara de cep
-function MascaraCep(cep){
-                if(mascaraInteiro(cep)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(cep, '00.000-000', event);
-}
-
 //adiciona mascara de data
 function MascaraData(data){
         if(mascaraInteiro(data)==false){
@@ -23,34 +15,12 @@ function MascaraData(data){
         return formataCampo(data, '00/00/0000', event);
 }
 
-//adiciona mascara ao telefone
-function MascaraTelefone(tel){  
-        if(mascaraInteiro(tel)==false){
-                event.returnValue = false;
-        }       
-        return formataCampo(tel, '(000) 00000-0000', event);
-}
-
 //adiciona mascara ao CPF
 function MascaraCPF(cpf){
         if(mascaraInteiro(cpf)==false){
                 event.returnValue = false;
         }       
         return formataCampo(cpf, '000.000.000-00', event);
-}
-
-//valida telefone
-function ValidaTelefone(tel){
-        exp = /\(\d{2}\)\ \d{4}\-\d{4}/
-        if(!exp.test(tel.value))
-                alert('Numero de Telefone Invalido!');
-}
-
-//valida CEP
-function ValidaCep(cep){
-        exp = /\d{2}\.\d{3}\-\d{3}/
-        if(!exp.test(cep.value))
-                alert('Numero de Cep Invalido!');               
 }
 
 //valida data
